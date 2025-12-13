f6e382e022d7a22a70b5ec4064d1f45441f970b1
baiju912 <baijuyadav0006@gmail.com>
2025-12-13 20:27:59 +0530

    Jokes Ka Pitara

---
+++ Advancejs/jokeapp/script.js
@@ -0,0 +1,16 @@
+async function getnewjoke()
+{
+
+
+    const response =await fetch("https://official-joke-api.appspot.com/jokes/random");
+
+    // console.log(response);
+
+    const data = await response.json();
+    // console.log(data);
+
+    document.getElementById("setup").innerText = data.setup;
+    document.getElementById("punchline").innerText = data.punchline;
+
+
+}
