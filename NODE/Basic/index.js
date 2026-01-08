283d087779042605ed74238f95d13af37f7f3b94
baiju912 <baijuyadav0006@gmail.com>
2026-01-08 16:21:55 +0530

    first code node

---
+++ NODE/Basic/index.js
@@ -0,0 +1,34 @@
+// console.log("Hello World!");
+
+// let a =12;
+//  if(a%2 === 0)
+//  {
+//     console.log("even");
+    
+//  }
+//  else{
+//     console.log("odd");
+    
+//  }
+
+function add(a,b)
+{
+    console.log(a+b);
+    
+}
+
+function sub(a,b)
+{
+    console.log(a - b);
+    
+}
+
+function mul(a,b)
+{
+    console.log(a * b);
+    
+}
+console.log("Baiju");
+
+export  {add, sub};
+export default  mul;
---
+++ NODE/Basic/index2.js
@@ -0,0 +1,12 @@
+import {add, sub} from "./index.js";
+
+import kuch from "./index.js";
+
+let x = 30;
+let y = 20;
+
+add(x,y);
+
+ sub(x,y);
+ kuch(x,y);
+
